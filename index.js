0c451d6241284b82b301d24fe33f7a5bad7ae448
Anuska86 <anasrm86@gmail.com>
2024-06-04 11:31:02 +0200

    Update

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-const PORT = process.env.PORT || 3001
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
 })
